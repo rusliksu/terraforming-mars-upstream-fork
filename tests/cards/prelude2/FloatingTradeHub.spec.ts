0caa52a0082b08e2cc97929710d9e92fc44823b7
rusliksu <25394128+rusliksu@users.noreply.github.com>
2026-07-07 15:44:48 +0300

    Clarify Floating Trade Hub tests

--- tests/cards/prelude2/FloatingTradeHub.spec.ts
+++ tests/cards/prelude2/FloatingTradeHub.spec.ts
@@ -24,70 +24,72 @@ describe('FloatingTradeHub', () => {
     player.playedCards.push(card);
   });
 
-  it('adds floaters immediately when Floating Trade Hub is the only target', () => {
-    cast(card.action(player), undefined);
+  describe('when converting floaters is not available', () => {
+    it('adds floaters immediately when Floating Trade Hub is the only target', () => {
+      cast(card.action(player), undefined);
 
-    expect(card.resourceCount).eq(2);
+      expect(card.resourceCount).eq(2);
+    });
+
+    it('prompts for a floater card when multiple targets are available', () => {
+      player.playedCards.push(newProjectCard(CardName.TARDIGRADES)!);
+      player.playedCards.push(newProjectCard(CardName.AERIAL_MAPPERS)!);
+      const selectCard = cast(card.action(player), SelectCard);
+
+      expect(selectCard.cards.map(toName)).deep.eq([card.name, CardName.AERIAL_MAPPERS]);
+
+      selectCard.cb([card]);
+
+      expect(card.resourceCount).eq(2);
+    });
   });
 
+  describe('when converting floaters is available', () => {
+    beforeEach(() => {
+      card.resourceCount = 5;
+    });
 
-  it('prompts for a floater card when multiple targets are available', () => {
-    player.playedCards.push(newProjectCard(CardName.TARDIGRADES)!);
-    player.playedCards.push(newProjectCard(CardName.AERIAL_MAPPERS)!);
-    const selectCard = cast(card.action(player), SelectCard);
+    it('Act - select resource', () => {
+      const orOptions = cast(card.action(player), OrOptions);
 
-    expect(selectCard.cards.map(toName)).deep.eq([card.name, CardName.AERIAL_MAPPERS]);
+      expect(orOptions.options).has.length(2);
+      const andOptions = cast(orOptions.options[1], AndOptions);
+      const selectAmount = cast(andOptions.options[0], SelectAmount);
+      const selectResource = cast(andOptions.options[1], SelectResource);
 
-    selectCard.cb([card]);
+      expect(selectAmount.min).eq(1);
+      expect(selectAmount.max).eq(5);
+      expect(selectResource.include).deep.eq(Units.keys);
 
-    expect(card.resourceCount).eq(2);
-  });
+      andOptions.process({type: 'and', responses: [
+        {type: 'amount', amount: 4},
+        {type: 'resource', resource: 'plants'},
+      ]}, player);
 
-  it('Act - select resource', () => {
-    card.resourceCount = 5;
+      expect(player.stock.asUnits()).deep.eq(Units.of({plants: 4}));
+      expect(card.resourceCount).to.eq(1);
+    });
 
-    const orOptions = cast(card.action(player), OrOptions);
+    it('Act - add resources branch autoselects the only target card', () => {
+      const orOptions = cast(card.action(player), OrOptions);
+      const selectOption = cast(orOptions.options[0], SelectOption);
 
-    expect(orOptions.options).has.length(2);
-    const andOptions = cast(orOptions.options[1], AndOptions);
-    const selectAmount = cast(andOptions.options[0], SelectAmount);
-    const selectResource = cast(andOptions.options[1], SelectResource);
+      selectOption.cb(undefined);
 
-    expect(selectAmount.min).eq(1);
-    expect(selectAmount.max).eq(5);
-    expect(selectResource.include).deep.eq(Units.keys);
+      expect(card.resourceCount).eq(7);
+    });
 
-    andOptions.process({type: 'and', responses: [
-      {type: 'amount', amount: 4},
-      {type: 'resource', resource: 'plants'},
-    ]}, player);
+    it('Act - add resources branch prompts when multiple targets are available', () => {
+      player.playedCards.push(newProjectCard(CardName.AERIAL_MAPPERS)!);
 
-    expect(player.stock.asUnits()).deep.eq(Units.of({plants: 4}));
-    expect(card.resourceCount).to.eq(1);
-  });
+      const orOptions = cast(card.action(player), OrOptions);
+      const selectCard = cast(orOptions.options[0], SelectCard);
 
-  it('Act - add resources branch autoselects the only target card', () => {
-    card.resourceCount = 5;
+      expect(selectCard.cards.map(toName)).deep.eq([card.name, CardName.AERIAL_MAPPERS]);
 
-    const orOptions = cast(card.action(player), OrOptions);
-    const selectOption = cast(orOptions.options[0], SelectOption);
+      selectCard.cb([card]);
 
-    selectOption.cb(undefined);
-
-    expect(card.resourceCount).eq(7);
-  });
-
-  it('Act - add resources branch prompts when multiple targets are available', () => {
-    card.resourceCount = 5;
-    player.playedCards.push(newProjectCard(CardName.AERIAL_MAPPERS)!);
-
-    const orOptions = cast(card.action(player), OrOptions);
-    const selectCard = cast(orOptions.options[0], SelectCard);
-
-    expect(selectCard.cards.map(toName)).deep.eq([card.name, CardName.AERIAL_MAPPERS]);
-
-    selectCard.cb([card]);
-
-    expect(card.resourceCount).eq(7);
+      expect(card.resourceCount).eq(7);
+    });
   });
 });

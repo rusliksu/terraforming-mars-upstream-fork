f017348c5d2b8dd5b2d40c12c02197b9f24c4fbf
rusliksu <25394128+rusliksu@users.noreply.github.com>
2026-07-11 21:02:34 +0300

    Skip Ecotec target prompt for single card

--- src/server/cards/prelude2/Ecotec.ts
+++ src/server/cards/prelude2/Ecotec.ts
@@ -10,6 +10,7 @@ import {OrOptions} from '../../inputs/OrOptions';
 import {SelectOption} from '../../inputs/SelectOption';
 import {SelectCard} from '../../inputs/SelectCard';
 import {ICorporationCard} from '../corporation/ICorporationCard';
+import {message} from '../../logs/MessageBuilder';
 
 export class Ecotec extends CorporationCard implements ICorporationCard {
   constructor() {
@@ -47,22 +48,27 @@ export class Ecotec extends CorporationCard implements ICorporationCard {
     }
 
     for (let i = 0; i < count; i++) {
-      player.defer(
-        () => new OrOptions(
-          new SelectCard(
-            'Select card to gain a microbe',
-            'Add microbe',
-            microbeCards)
-            .andThen(([card]) => {
-              player.addResourceTo(card, {qty: 1, log: true});
-              return undefined;
-            }),
+      const gainPlant = new SelectOption('Gain plant').andThen(() => {
+        player.stock.add(Resource.PLANTS, 1, {log: true});
+        return undefined;
+      });
 
-          new SelectOption('Gain plant').andThen(() => {
-            player.stock.add(Resource.PLANTS, 1, {log: true});
+      const addMicrobe = microbeCards.length === 1 ?
+        new SelectOption(message('Add microbe to ${0}', (b) => b.card(microbeCards[0])), 'Add microbe').andThen(() => {
+          player.addResourceTo(microbeCards[0], {qty: 1, log: true});
+          return undefined;
+        }) :
+        new SelectCard(
+          'Select card to gain a microbe',
+          'Add microbe',
+          microbeCards)
+          .andThen(([card]) => {
+            player.addResourceTo(card, {qty: 1, log: true});
             return undefined;
-          }),
-        ),
+          });
+
+      player.defer(
+        () => new OrOptions(addMicrobe, gainPlant),
       );
     }
   }
